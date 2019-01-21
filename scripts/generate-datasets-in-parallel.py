2dc70b3f4d79800c617611056cdefb1101f50bca
Jost Migenda <jmigenda1@sheffield.ac.uk>
2019-01-21 16:21:08 +0000

    added code to parallelize more steps

--- scripts/generate-datasets-in-parallel.py
+++ scripts/generate-datasets-in-parallel.py
@@ -1,19 +1,26 @@
+import argparse
 from multiprocessing import Pool
 import os
 import subprocess
 from time import sleep
 
-# TODO: add more files: Garching directions, Nakazato masses/metallicities
-inputs = (("infile_full", "garching"),
-#           ("infile_intp2002.txt", "nakazato"),
-          ("infile_wilson", "totani")
+parser = argparse.ArgumentParser()
+parser.add_argument("step", help="What step of the analysis chain to do. \
+                                  Can be 'generate_dataset', 'reconstruct_energy' or 'readd_time'.")
+args = parser.parse_args()
+
+# input files: one per model family (to study explosion mechanism)
+inputs = (
+          ("in_tamborra27", "garching"),
+          ("in_spectob2000.data", "nakazato"),
+          ("in_totani20", "totani"),
          )
 
-nevts = (300, 1000, 3000)#, 10000, 30000)
+nevts = (3e5,) #(300, 1000, 3000, 10000, 30000)
 
 hierarchies = ("noosc", "normal", "inverted")
 
-imax = 5
+imax = 1
 
 def generate_dataset(input, nevt, hierarchy, i):
     print "... now running:", input, str(nevt), hierarchy, str(i)
@@ -35,6 +42,57 @@ def generate_dataset(input, nevt, hierarchy, i):
     result = subprocess.call(cmd, shell=True)
     return result
 
+
+def reconstruct_energy(input, nevt, hierarchy, i):
+    print "... now reconstructing:", input, str(nevt), hierarchy, str(i)
+    infile, format = input
+    _infile = infile.split('.')[0] # remove file extension, if any
+    path = "out/%s/%s" % (nevt, format)
+    outfile = "%s/%s-%s-%s" % (path, _infile, hierarchy, i)
+
+    if not os.path.isdir(path):
+        print("Directory %s doesn't exist. Aborting ..." % path)
+        raise Exception
+    elif os.path.isfile(outfile + ".reco"):
+        print('File %s.reco already exists. Aborting to avoid overwriting it ...' % outfile)
+        raise Exception
+
+    cmd = "root -q '../energetic-bonsai/energetic_bonsai_final.C(\"%s.root\")' > %s.reco" % (outfile, outfile)
+    print cmd
+    result = subprocess.call(cmd, shell=True)
+    return result
+
+
+def readd_time(input, nevt, hierarchy, i):
+    infile, format = input
+    _infile = infile.split('.')[0] # remove file extension, if any
+    path = "out/%s/%s" % (nevt, format)
+    outfile = "%s/%s-%s-%s" % (path, _infile, hierarchy, i)
+
+    if not os.path.isfile(outfile + '.reco'):
+        print("File %s.reco doesn't exist. Aborting ..." % outfile)
+        raise Exception
+    elif os.path.isfile(outfile + '.reco2'):
+        print('File %s.reco2 already exists. Aborting to avoid overwriting it ...' % outfile)
+        raise Exception
+
+
+    cmd = "python ../sntools/scripts/add-time-to-reconstructed-evts.py %s" % outfile
+    print cmd
+    result = subprocess.call(cmd, shell=True)
+    return result
+
+
+if args.step == 'generate_dataset':
+    f = generate_dataset
+elif args.step == 'reconstruct_energy':
+    f = reconstruct_energy
+elif args.step == 'readd_time':
+    f = readd_time
+else:
+    raise Exception("%s: No such function." % args.step)
+
+
 pool = Pool(processes=4) # start 4 worker processes
 
 results = []
@@ -43,7 +101,7 @@ for input in inputs:
         for hierarchy in hierarchies:
             for i in range(1, imax+1):
                 print "Adding to pool:", input, str(nevt), hierarchy, str(i)
-                res = pool.apply_async(generate_dataset, (input, nevt, hierarchy, i))
+                res = pool.apply_async(f, (input, nevt, hierarchy, i))
                 results.append(res)
                 sleep(1) # this appears to fix an issue where the pool would skip some of the first tasks
 
