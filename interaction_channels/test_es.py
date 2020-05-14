9ee8f86b3fb8168bacca7ea0a4ab2131463094c9
Jost Migenda <jost.migenda@kcl.ac.uk>
2020-05-14 12:53:58 +0200

    Cleaned up interaction channel tests Reduce duplicate code by separating test code (in shared parent class) and numerical values (in separate child classes for each channel).

--- interaction_channels/test_es.py
+++ interaction_channels/test_es.py
@@ -5,79 +5,69 @@ except:
     import builtins # Python 3
 import unittest
 
-from . import es as ibd # 😅
+from . import es
+from ._crosssectiontest import CrossSectionTest
 
 builtins._flavor = 'e'
 
-class CrossSectionTest(unittest.TestCase):
-    # test a few sensible values
-    def test_dSigma_dE(self):
-        test_values = (
-                       ( 3,  1.7, 2.3427673889264988e-23),
-                       (20, 18.0, 2.2641725887270564e-23),
-                       (20, 18.4, 2.2579850452106914e-23),
-                       (50, 48.7, 2.2212735153636973e-23),
-                       (100,97.0, 2.216109309506134e-23)
-                      )
-        for (eNu, eE, result) in test_values:
-            self.assertAlmostEqual(ibd.dSigma_dE(eNu, eE), result, delta=1e-6*result)
 
+class ESTest(CrossSectionTest):
+    c = es  # ensure we can access interaction channel module as self.c
 
-    def test_dSigma_dE_edgecases(self):
-        # test unphysical values
-        self.assertEqual(ibd.dSigma_dE(ibd.bounds_eNu[0] - 1e-6, ibd.eE_min), 0) # eNu too small
-        self.assertEqual(ibd.dSigma_dE(20, ibd.eE_min - 1e-6), 0) # eE too small
-        self.assertEqual(ibd.dSigma_dE(20, 20.259), 0) # eE too large
+    # iterable with tuples (eNu, eE, dSigma_dE(eNu, eE))
+    test_dSigma_dE_values = (
+        (3,  1.7, 2.3427673889264988e-23),
+        (20, 18.0, 2.2641725887270564e-23),
+        (20, 18.4, 2.2579850452106914e-23),
+        (50, 48.7, 2.2212735153636973e-23),
+        (100, 97.0, 2.216109309506134e-23)
+    )
 
-    def test_dSigma_dCosT(self):
-        test_values = (
-                       ( 3, -0.50, 0.0),
-                       ( 3,  0.00, 0.0),
-                       ( 3,  0.50, 0.0),
-                       (20, -0.99, 0.0),
-                       (20,  0.00, 0.0),
-                       (20,  0.99, 9.55532554942379e-21),
-                       (50, -0.50, 0.0),
-                       (50,  0.00, 0.0),
-                       (50,  0.50, 4.4738155338186293e-23),
-                      )
-        for (eNu, cosT, result) in test_values:
-            self.assertAlmostEqual(ibd.dSigma_dCosT(eNu, cosT), result, delta=1e-6*result)
+    # iterable with tuples (eNu, eE, dSigma_dE(eNu, eE))
+    test_dSigma_dE_edgecases_values = (
+        (c.bounds_eNu[0] - 1e-6, c.eE_min, 0),  # eNu too small
+        (20, c.eE_min - 1e-6, 0), # eE too small
+        (20, 20.259, 0), # eE too large
+    )
 
-    def test_dSigma_dCosT_edgecases(self):
-        # unphysical values of cos(theta)
-        self.assertEqual(ibd.dSigma_dCosT(20, 1.01), 0)
-        self.assertEqual(ibd.dSigma_dCosT(20, -1.01), 0)
+    # iterable with tuples (eNu, cosT, dSigma_dE(eNu, cosT))
+    test_dSigma_dCosT_values = (
+        (3, -0.50, 0.0),
+        (3,  0.00, 0.0),
+        (3,  0.50, 0.0),
+        (20, -0.99, 0.0),
+        (20,  0.00, 0.0),
+        (20,  0.99, 9.55532554942379e-21),
+        (50, -0.50, 0.0),
+        (50,  0.00, 0.0),
+        (50,  0.50, 4.4738155338186293e-23),
+    )
 
-    def test_get_eE(self):
-        test_values = (
-                       ( 3, -0.50, 0.7391887582525531),
-                       ( 3,  0.00, 0.5109989),
-                       ( 3,  0.50, 0.7391887582525531),
-                       (20, -0.99, 14.490377038079016),
-                       (20,  0.00, 0.5109989),
-                       (20,  0.99, 14.490377038079016),
-                       (50, -0.50, 0.8425819323212276),
-                       (50,  0.00, 0.5109989),
-                       (50,  0.50, 0.8425819323212276),
-                      )
-        for (eNu, cosT, result) in test_values:
-            self.assertAlmostEqual(ibd.get_eE(eNu, cosT), result)
+    # iterable with tuples (eNu, cosT, get_eE(eNu, cosT))
+    test_get_eE_values = (
+        (3, -0.50, 0.7391887582525531),
+        (3,  0.00, 0.5109989),
+        (3,  0.50, 0.7391887582525531),
+        (20, -0.99, 14.490377038079016),
+        (20,  0.00, 0.5109989),
+        (20,  0.99, 14.490377038079016),
+        (50, -0.50, 0.8425819323212276),
+        (50,  0.00, 0.5109989),
+        (50,  0.50, 0.8425819323212276),
+    )
 
-    def test_bounds_eE(self):
-        test_values = (
-                       ( 3, 0.77, 3.275551663171255),
-                       (20, 0.77, 20.258722276922214),
-                       (50, 0.77, 50.25679841169901),
-                      )
-        for (eNu, eE_min, eE_max) in test_values:
-            self.assertAlmostEqual(ibd.bounds_eE(eNu)[0], eE_min)
-            self.assertAlmostEqual(ibd.bounds_eE(eNu)[1], eE_max)
+    # iterable with tuples (eNu, bounds_eE(eNu)[0], bounds_eE(eNu)[1])
+    test_bounds_eE_values = (
+        (3, 0.77, 3.275551663171255),
+        (20, 0.77, 20.258722276922214),
+        (50, 0.77, 50.25679841169901),
+    )
 
-    def test_bounds_eNu(self):
-        self.assertAlmostEqual(ibd.bounds_eNu[0], 0.4175023400112732)
-        self.assertEqual(ibd.bounds_eNu[1], 100)
+    # value of bounds_eNu[0]
+    test_bounds_eNu_minvalue = 0.4175023400112732
 
+# ensure that unittest doesn't run tests in the base class, via https://stackoverflow.com/a/22836015
+del(CrossSectionTest)
 
 if __name__ == '__main__':
     unittest.main()
--- interaction_channels/test_ibd.py
+++ interaction_channels/test_ibd.py
@@ -1,77 +1,66 @@
 import unittest
 
 from . import ibd
-
-class CrossSectionTest(unittest.TestCase):
-    # test a few sensible values
-    def test_dSigma_dE(self):
-        test_values = (
-                       ( 3,  1.7, 6.597590600586344e-20),
-                       (20, 18.0, 9.756791751947533e-20),
-                       (20, 18.4, 9.686960987505651e-20),
-                       (50, 48.7, 1.003350542600863e-19),
-                       (100,97.0, 9.302012628854371e-20)
-                      )
-        for (eNu, eE, result) in test_values:
-            self.assertAlmostEqual(ibd.dSigma_dE(eNu, eE), result, delta=1e-6*result)
+from ._crosssectiontest import CrossSectionTest
 
 
-    def test_dSigma_dE_edgecases(self):
-        # test unphysical values
-        self.assertEqual(ibd.dSigma_dE(ibd.eThr - 1e-6, ibd.mE), 0) # eNu too small
-        self.assertEqual(ibd.dSigma_dE(20, 17.941), 0) # eE too small
-        self.assertEqual(ibd.dSigma_dE(20, 18.706), 0) # eE too large
+class IBDTest(CrossSectionTest):
+    c = ibd  # ensure we can access interaction channel module as self.c
 
-    def test_dSigma_dCosT(self):
-        test_values = (
-                       ( 3, -0.50, 3.520263968746167e-22),
-                       ( 3,  0.00, 3.381936718733454e-22),
-                       ( 3,  0.50, 3.242202950723807e-22),
-                       (20, -0.99, 3.583761750083326e-20),
-                       (20,  0.00, 3.705455845604023e-20),
-                       (20,  0.99, 3.852469055868431e-20),
-                       (50, -0.50, 1.736535165001988e-19),
-                       (50,  0.00, 1.970150743424877e-19),
-                       (50,  0.50, 2.255441012065604e-19),
-                      )
-        for (eNu, cosT, result) in test_values:
-            self.assertAlmostEqual(ibd.dSigma_dCosT(eNu, cosT), result, delta=1e-6*result)
+    # iterable with tuples (eNu, eE, dSigma_dE(eNu, eE))
+    test_dSigma_dE_values = (
+        (3,  1.7, 6.597590600586344e-20),
+        (20, 18.0, 9.756791751947533e-20),
+        (20, 18.4, 9.686960987505651e-20),
+        (50, 48.7, 1.003350542600863e-19),
+        (100, 97.0, 9.302012628854371e-20)
+    )
 
-    def test_dSigma_dCosT_edgecases(self):
-        # unphysical values of cos(theta)
-        self.assertEqual(ibd.dSigma_dCosT(20, 1.01), 0)
-        self.assertEqual(ibd.dSigma_dCosT(20, -1.01), 0)
+    # iterable with tuples (eNu, eE, dSigma_dE(eNu, eE))
+    test_dSigma_dE_edgecases_values = (
+        (c.eThr - 1e-6, c.mE, 0),  # eNu too small
+        (20, 17.841, 0),  # eE too small
+        (20, 18.706, 0),  # eE too large
+    )
 
-    def test_get_eE(self):
-        test_values = (
-                       ( 3, -0.50, 1.6979103666324273),
-                       ( 3,  0.00, 1.700490693779404),
-                       ( 3,  0.50, 1.7030796578148266),
-                       (20, -0.99, 17.944888098364295),
-                       (20,  0.00, 18.31551834516354),
-                       (20,  0.99, 18.701794009695444),
-                       (50, -0.50, 45.100902584113804),
-                       (50,  0.00, 46.24173207227394),
-                       (50,  0.50, 47.44178162206098),
-                      )
-        for (eNu, cosT, result) in test_values:
-            self.assertAlmostEqual(ibd.get_eE(eNu, cosT), result)
+    # iterable with tuples (eNu, cosT, dSigma_dE(eNu, cosT))
+    test_dSigma_dCosT_values = (
+        (3, -0.50, 3.520263968746167e-22),
+        (3,  0.00, 3.381936718733454e-22),
+        (3,  0.50, 3.242202950723807e-22),
+        (20, -0.99, 3.583761750083326e-20),
+        (20,  0.00, 3.705455845604023e-20),
+        (20,  0.99, 3.852469055868431e-20),
+        (50, -0.50, 1.736535165001988e-19),
+        (50,  0.00, 1.970150743424877e-19),
+        (50,  0.50, 2.255441012065604e-19),
+    )
 
-    def test_bounds_eE(self):
-        test_values = (
-                       ( 3, 1.695338635167926, 1.705677300207871),
-                       (20, 17.941220926704954, 18.70577898514169),
-                       (50, 44.01501537362355, 48.70578493879961),
-                      )
-        for (eNu, eE_min, eE_max) in test_values:
-            self.assertAlmostEqual(ibd.bounds_eE(eNu)[0], eE_min)
-            self.assertAlmostEqual(ibd.bounds_eE(eNu)[1], eE_max)
+    # iterable with tuples (eNu, cosT, get_eE(eNu, cosT))
+    test_get_eE_values = (
+        (3, -0.50, 1.6979103666324273),
+        (3,  0.00, 1.700490693779404),
+        (3,  0.50, 1.7030796578148266),
+        (20, -0.99, 17.944888098364295),
+        (20,  0.00, 18.31551834516354),
+        (20,  0.99, 18.701794009695444),
+        (50, -0.50, 45.100902584113804),
+        (50,  0.00, 46.24173207227394),
+        (50,  0.50, 47.44178162206098),
+    )
 
-    def test_bounds_eNu(self):
-        self.assertAlmostEqual(ibd.eThr, 1.8060455572837861)
-        self.assertEqual(ibd.bounds_eNu[0], ibd.eThr)
-        self.assertEqual(ibd.bounds_eNu[1], 100)
+    # iterable with tuples (eNu, bounds_eE(eNu)[0], bounds_eE(eNu)[1])
+    test_bounds_eE_values = (
+        (3, 1.695338635167926, 1.705677300207871),
+        (20, 17.941220926704954, 18.70577898514169),
+        (50, 44.01501537362355, 48.70578493879961),
+    )
 
+    # value of bounds_eNu[0]
+    test_bounds_eNu_minvalue = 1.8060455572837861
+
+# ensure that unittest doesn't run tests in the base class, via https://stackoverflow.com/a/22836015
+del(CrossSectionTest)
 
 if __name__ == '__main__':
     unittest.main()
