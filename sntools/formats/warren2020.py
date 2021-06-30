0a52e98db4f8336dd6e399e8d03df2bf7a89f6a2
Jost Migenda <jost.migenda@kcl.ac.uk>
2021-06-30 21:42:54 +0200

    formats: turn global vars into class instance vars

--- sntools/formats/warren2020.py
+++ sntools/formats/warren2020.py
@@ -9,55 +9,43 @@ import h5py
 
 from sntools.formats import gamma, get_starttime, get_endtime
 
-flux = {}
 
+class Flux(gamma.Flux):
+    # Note: prepare_evt_gen and nu_emission are inherited from gamma.Flux
 
-def parse_input(input, inflv, starttime, endtime):
-    """Read simulations data from input file.
+    def parse_input(self, input, inflv, starttime, endtime):
+        """Read simulations data from input file.
 
-    Arguments:
-    input -- prefix of file containing neutrino fluxes
-    inflv -- neutrino flavor to consider
-    starttime -- start time set by user via command line option (or None)
-    endtime -- end time set by user via command line option (or None)
-    """
+        Arguments:
+        input -- prefix of file containing neutrino fluxes
+        inflv -- neutrino flavor to consider
+        starttime -- start time set by user via command line option (or None)
+        endtime -- end time set by user via command line option (or None)
+        """
 
-    f = h5py.File(input, 'r')
-    for (t, r) in f['sim_data']['shock_radius']:
-        if r > 1:
-            tbounce = t * 1000  # convert to ms
-            break
+        f = h5py.File(input, 'r')
+        for (t, r) in f['sim_data']['shock_radius']:
+            if r > 1:
+                tbounce = t * 1000  # convert to ms
+                break
 
-    starttime = get_starttime(starttime, 1000 * f['sim_data']['shock_radius'][0][0] - tbounce)
-    endtime = get_endtime(endtime, 1000 * f['sim_data']['shock_radius'][-1][0] - tbounce)
+        starttime = get_starttime(starttime, 1000 * f['sim_data']['shock_radius'][0][0] - tbounce)
+        endtime = get_endtime(endtime, 1000 * f['sim_data']['shock_radius'][-1][0] - tbounce)
 
-    # Save flux data to dictionary to look up in nu_emission() below
-    global flux
-    flux = {}
-    path = {'e': 'nue_data', 'eb': 'nuae_data', 'x': 'nux_data', 'xb': 'nux_data'}[inflv]
-    for i, (t, lum) in enumerate(f[path]['lum']):
-        t = 1000 * t - tbounce  # convert to time post-bounce in ms
-        if (t < starttime - 30) or (t > endtime + 30):
-            # Ignore data outside of the requested time span.
-            continue
+        # Save flux data to dictionary to look up in nu_emission() below
+        self.flux = {}
+        path = {'e': 'nue_data', 'eb': 'nuae_data', 'x': 'nux_data', 'xb': 'nux_data'}[inflv]
+        for i, (t, lum) in enumerate(f[path]['lum']):
+            t = 1000 * t - tbounce  # convert to time post-bounce in ms
+            if (t < starttime - 30) or (t > endtime + 30):
+                # Ignore data outside of the requested time span.
+                continue
 
-        lum *= 1e51 * 624.151  # convert from 10^51 erg/s to MeV/ms
-        mean_e = f[path]['avg_energy'][i][1]
-        mean_e_sq = f[path]['rms_energy'][i][1]**2
+            lum *= 1e51 * 624.151  # convert from 10^51 erg/s to MeV/ms
+            mean_e = f[path]['avg_energy'][i][1]
+            mean_e_sq = f[path]['rms_energy'][i][1]**2
 
-        flux[t] = (mean_e, mean_e_sq, lum)
+            self.flux[t] = (mean_e, mean_e_sq, lum)
 
-    f.close()
-    return (starttime, endtime, sorted(flux.keys()))
-
-
-def prepare_evt_gen(binned_t):
-    global flux
-    gamma.flux = flux
-    gamma.prepare_evt_gen(binned_t)
-    flux = gamma.flux
-
-
-def nu_emission(eNu, time):
-    gamma.flux = flux
-    return gamma.nu_emission(eNu, time)
+        f.close()
+        return (starttime, endtime, sorted(self.flux.keys()))
