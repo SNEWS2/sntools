549dd9ff8b4a2c4e0f0f496888c308f48bf408ee
Jost Migenda <jost.migenda@kcl.ac.uk>
2021-08-11 01:47:31 +0200

    Release version 1.0b2

--- sntools/__init__.py
+++ sntools/__init__.py
@@ -12,7 +12,7 @@ For usage information, run `sntools -h` or `python sntools/genevts.py -h`.
 For more extensive documentation, to report issues or to contribute code,
 see https://github.com/JostMigenda/sntools.
 """
-__version__ = '1.0b1'
+__version__ = '1.0b2'
 
 
 def setup():
