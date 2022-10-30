7895a45f229ce84996761d493af6344124c8e1ce
Susanna Wakely <susannawakely@gmail.com>
2022-10-30 16:33:40 +0000

    Removed all gamma emission code. Replaced table of data for all emission channels with data of relevant emission channel only. The fit()(eNu) function currently returns an array of the cross-section value rather than just a number.

--- sntools/interaction_channels/o16nc_p.py
+++ sntools/interaction_channels/o16nc_p.py
@@ -3,9 +3,8 @@
     
 Based on data provided in Suzuki et al. 2018 (Phys. Rev. C 98, 034613).
 A spline fit is used to obtain cross-section as a function of neutrino energy.
-Some data points have been estimated from a by-eye fit in order to create a 
-smooth spline fit. The threshold energy of the interaction was also estimated 
-from a by-eye fit as no explicit threshold energy could be found.
+The threshold energy of the interaction is estimated from a by-eye fit as no 
+explicit threshold energy could be found.
 
 To determine the the differential cross section dSigma/dE (eNu, eE) from the
 total cross section, we approximate a DiracDelta function with one that is
@@ -15,81 +14,25 @@ total cross section, we approximate a DiracDelta function with one that is
 from sntools.event import Event
 from sntools.interaction_channels import BaseChannel
 from scipy.interpolate import interp1d
-# import numpy as np
 import random
 
-""" This table shows the partial cross-section of 16O(nu, nu' X). It is taken 
-directly from Suzuki et al. 2018 with an estimated threshold energy data point to
-ensure a smooth spline fit. (cross-section units 10^-42 cm^2)
-(eNu, gamma, n, p, d, pp, 3H, 3He, alpha, alpha_n, alpha_p, total)
-"""
-
-e_thr = 14  # approximate energy threshold of proton emission with no gamma emission (MeV)
+e_thr = 14.0  # approximate energy threshold of proton emission
 mP = 938.3  # proton mass (MeV)
-'''e_thr_g = e_thr + 6.32  # energy threshold of proton emission with gamma emission (MeV)'''
 epsilon = 0.001  # for approximating DiracDelta distribution below
 
 # List of neutrino flavors ("e", "eb", "x", "xb") that interact in this channel.
 possible_flavors = ("e", "eb", "x", "xb")
-
-x_sec_data = [(e_thr, None    , None    , 0.0     , None    , None    , None    , None    ,
-                           None    , None    , None    , None    ), #Estimated Threshold Energy
-              (15.0, 1.35E-07, None    , 6.05E-04, None    , None    , None    , None    ,
-                           1.35E-25, None    , None    , 6.05E-04),
-              (20.0, 1.02E-05, 1.93E-04, 3.08E-02, 0.0     , 0.0     , 0.0     , 0.0     ,
-                           1.47E-10, None    , 0.0     , 3.10E-02), 
-              (25.0, 9.97E-05, 1.63E-02, 1.98E-01, 2.88E-05, 1.09E-05, 2.46E-09, 1.82E-05,
-                           1.74E-06, 0.0     , 4.30E-08, 2.14E-01), 
-              (30.0, 5.54E-04, 1.29E-01, 7.76E-01, 7.92E-03, 5.71E-03, 1.82E-03, 4.19E-03,
-                           4.14E-03, 3.76E-05, 4.83E-03, 9.34E-01), 
-              (35.0, 1.83E-03, 4.80E-01, 2.16E+00, 6.43E-02, 4.74E-02, 1.48E-02, 3.56E-02,
-                           3.17E-02, 6.86E-04, 5.39E-02, 2.89E+00), 
-              (40.0, 4.46E-03, 1.22E+00, 4.76E+00, 2.06E-01, 1.51E-01, 4.52E-02, 1.18E-01,
-                           9.55E-02, 3.74E-03, 1.99E-01, 6.80E+00), 
-              (45.0, 9.00E-03, 2.49E+00, 9.01E+00, 4.68E-01, 3.41E-01, 9.96E-02, 2.75E-01,
-                           2.07E-01, 1.09E-02, 4.96E-01, 1.34E+01), 
-              (50.0, 1.60E-02, 4.44E+00, 1.52E+01, 8.87E-01, 6.44E-01, 1.85E-01, 5.36E-01, 
-                           3.81E-01, 2.43E-02, 9.97E-01, 2.33E+01), 
-              (55.0, 2.60E-02, 7.17E+00, 2.37E+01, 1.49E+00, 1.08E+00, 3.06E-01, 9.23E-01, 
-                           6.27E-01, 4.53E-02, 1.75E+00, 3.71E+01), 
-              (60.0, 3.94E-02, 1.07E+01, 3.45E+01, 2.31E+00, 1.68E+00, 4.69E-01, 1.45E+00, 
-                           9.57E-01, 7.48E-02, 2.80E+00, 5.50E+01), 
-              (65.0, 5.66E-02, 1.52E+01, 4.77E+01, 3.36E+00, 2.43E+00, 6.75E-01, 2.14E+00, 
-                           1.37E+00, 1.13E-01, 4.17E+00, 7.72E+01), 
-              (70.0, 7.78E-02, 2.04E+01, 6.29E+01, 6.64E+00, 3.35E+00, 9.24E-01, 2.97E+00, 
-                           1.87E+00, 1.60E-01, 5.85E+00, 1.05E+02), 
-              (80.0, 1.34E-01, 3.28E+01, 9.86E+01, 7.82E+00, 5.63E+00, 1.54E+00, 5.06E+00, 
-                           3.11E+00, 2.78E-01, 1.01E+01, 1.65E+02), 
-              (90.0, 2.10E-01, 4.68E+01, 1.38E+02, 1.17E+01, 8.36E+00, 2.27E+00, 7.57E+00, 
-                           4.58E+00, 4.23E-01, 1.53E+01, 2.35E+02)] 
-
-gam, n, p, d, pp, H3, He3, alp, alp_n, alp_p, tot=[],[],[],[],[],[],[],[],[],[],[] 
-a = (gam, n, p, d, pp, H3, He3, alp, alp_n, alp_p, tot)
-for j in range(1, 12):   
-    # sorting through the data removing any 'None' data points and returning
-    e_Nu = []
-    s = [] # a list of two lists  of the form [energy, cross-section for given final state]
-    for i in range(0, len(x_sec_data)):
-        if x_sec_data[i][j] is None:    continue
-        else:
-            s.append(x_sec_data[i][j])
-            e_Nu.append(x_sec_data[i][0])
-    a[j-1].append(e_Nu)
-    a[j-1].append(s)
     
-data = {'gamma':gam, 'neutron':n, 'proton':p, 'deuteron':d, # Dictionary of data 
-        'pp':pp, 'H3':H3, 'He3':He3, 'alpha':alp,   # sets for the final states
-        'alpha_n':alp_n, 'alpha_p':alp_p, 'total':tot}
-
-def fit(emission):
-    """ Return a spline fit for the given emission's partial cross-section as 
+data = [[e_thr, 15.0, 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0, 65.0, 70.0, 80.0, 90.0],
+ [0.0, 0.000605, 0.0308, 0.198, 0.776, 2.16, 4.76, 9.01, 15.2, 23.7, 34.5, 47.7, 62.9, 98.6, 138.0]]
+ #list of energies and cross-sections for o16nc proton emission taken from Suzuki et al 2018.
+ 
+def fit():
+    """ Return a spline fit for the proton emission partial cross-section as 
     a function of energy.
-    
-    Input:
-        emission: data dictionary key for partial cross-section to be fitted
     """
-    eNu = data[emission][0]
-    f = interp1d(eNu, data[emission][1], kind='cubic', 
+    eNu = data[0]
+    f = interp1d(eNu, data[1], kind='cubic', 
                  fill_value='extrapolate', bounds_error = False)
     return f
 
@@ -111,29 +54,7 @@ class Channel(BaseChannel):
         evt.incoming_particles.append((8016, 14900, 0, 0, 1))  # oxygen-16 nucleus at rest
         evt.outgoing_particles.append([2212, eE, dirx, diry, dirz])  # emitted proton
         # evt.outgoing_particles.append([nu_flv, eNu-e_thr, 0, 0, 1])  # outgoing nu
-        
-        '''if self.gamma(eNu) is True: #excited O-16 nucleus emits a proton and a photon
-            g_dirx, g_diry, g_dirzN = self.get_photon_direction()
-            evt.outgoing_particles.append([22, 6.32, g_dirx, g_diry, g_dirzN]) # emitted gamma
-            evt.outgoing_particles.append([2212, eE, dirx, diry, dirz])  # emitted proton
-        else:           # excited O-16 nucleus emits just a proton
-            evt.outgoing_particles.append([2212, eE, dirx, diry, dirz])  # emitted proton'''
         return evt
-
-    '''def gamma(self, eNu):
-        """Returns True if a gamma emission occurs.
-        If eNu > e_thr_g, then the N-15 nucleus is left in the excited state in 
-        ~70% of interactions.
-        Therefore a de-excitation gamma is produced in addition to the knocked-out proton.
-        
-        Input:
-            eNu: neutrino energy
-        """
-        r = random.random()
-        if eNu > e_thr_g and r > 0.310107949:
-            return True
-        else: 
-            return False'''
     
     def bounds_eE(self, eNu, *args):
         """Return kinematic bounds for integration over eE.
@@ -153,30 +74,8 @@ class Channel(BaseChannel):
             eNu:  neutrino energy (in MeV)
             cosT: cosine of the angle between neutrino and outgoing (detected) particle
         """
-        eE = random.random()*(eNu-e_thr) + mP
-        '''if self.gamma(eNu) is True:
-            eE = random.random()*(eNu - e_thr_g) + mP # energy of emitted proton generated randomly 
-                                #from energy excess of neutrino over gamma emission threshold energy
-        else: eE = random.random()*(eNu-e_thr) + mP   # energy of emitted proton generated randomly 
-                                #from energy excess of neutrino over threshold energy  '''         
+        eE = random.random()*(eNu-e_thr) + mP    
         return eE
-
-    '''def get_photon_direction(self):
-        """
-        In the case where nucleon emission leaves the daughter nucleus in an
-        excited state, a photon is emitted to de-excite the nucleus.
-        
-        This function returns a random direction (normalised to 1) for this 
-        photon emission.
-        """
-        phi = 2*np.pi*random.random()
-        theta = np.pi*random.random()
-        
-        dirx = np.sin(theta)*np.cos(phi)
-        diry = np.sin(theta)*np.sin(phi)
-        dirz = np.cos(theta)
-        
-        return (dirx,diry,dirz)'''
     
     def dSigma_dE(self, eNu, eE):
         """Return differential cross section in MeV^-2.
@@ -190,7 +89,7 @@ class Channel(BaseChannel):
             # ... but just in case:
             return 0
 
-        sigma = fit('proton')(eNu)*10**(-42)  # cross-section at eNu from the interp1d 
+        sigma = fit()(eNu)*10**(-42)  # cross-section at eNu from the interp1d 
                                             # fit of Suzuki et al. 2018 data (units cm^2)
         sigma *= (5.067731E10)**2  # convert cm^2 to MeV^-2: 
             # http://www.wolframalpha.com/input/?i=cm%2F(hbar+*+c)+in+MeV%5E(-1)
@@ -215,4 +114,3 @@ class Channel(BaseChannel):
     def _bounds_eNu(self, eE):
         """Min/max neutrino energy that can produce a given proton energy."""
         return (e_thr + eE - mP, 100)
-
