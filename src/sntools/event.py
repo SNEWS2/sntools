3aece0b2ef83ab4adf758df466659e8731019354
Jost Migenda <jost.migenda@kcl.ac.uk>
2026-03-19 13:45:38 +0000

    unify _write_*_events functions for all formats

--- src/sntools/event.py
+++ src/sntools/event.py
@@ -40,21 +40,15 @@ class EventWriter:
         """Write list of Event objects to output file."""
         match self.format:
             case 'NUANCE':
-                for (i, evt) in enumerate(events):
-                    self.outfile.write(self._nuance_string(evt, i))
-                self.outfile.write("$ stop\n")
+                self._write_nuance_events(events)
             case 'RATPAC':
-                for (i, evt) in enumerate(events):
-                    self.outfile.write(self._ratpac_string(evt, i, events))
+                self._write_ratpac_events(events)
             case 'ROOT_JUNO':
-                self.outfile.mktree("SNEvents",{"nparticles": "uint64", "origPDGID":"int32", "nuE":"double", "pdgid": ("int32",(2,)),"t": ("float64",(2,)),
-                                                "px": ("float64",(2,)),"py":("float64",(2,)),"pz":("float64",(2,)),"m":("float64",(2,)), "channel": "int64"})
-                for (i, evt) in enumerate(events):
-                    self._juno_string(evt, i, self.outfile)
+                self._write_juno_events(events)
         
         self.outfile.close()
 
-    def _nuance_string(self, evt, i):
+    def _write_nuance_events(self, events):
         """Return NUANCE-formatted representation of event for writing to output file.
 
         Input:
@@ -63,18 +57,22 @@ class EventWriter:
         Output:
             String describing event."""
 
-        s = "$ begin\n"
-        s += f"$ nuance {evt.code}\n"
-        s += f"$ vertex {evt.vertex[0]:.5f} {evt.vertex[1]:.5f} {evt.vertex[2]:.5f} {evt.time:.8f}\n"
-        for (pid, e, dirx, diry, dirz) in evt.incoming_particles:
-            s += f"$ track {pid} {e:.5f} {dirx:.5f} {diry:.5f} {dirz:.5f} -1\n"
-        s += f"$ info 0 0 {i}\n"
-        for (pid, e, dirx, diry, dirz) in evt.outgoing_particles:
-            s += f"$ track {pid} {e:.5f} {dirx:.5f} {diry:.5f} {dirz:.5f} 0\n"
-        s += "$ end\n"
-        return s
+        for (i, evt) in enumerate(events):
+            s = "$ begin\n"
+            s += f"$ nuance {evt.code}\n"
+            s += f"$ vertex {evt.vertex[0]:.5f} {evt.vertex[1]:.5f} {evt.vertex[2]:.5f} {evt.time:.8f}\n"
+            for (pid, e, dirx, diry, dirz) in evt.incoming_particles:
+                s += f"$ track {pid} {e:.5f} {dirx:.5f} {diry:.5f} {dirz:.5f} -1\n"
+            s += f"$ info 0 0 {i}\n"
+            for (pid, e, dirx, diry, dirz) in evt.outgoing_particles:
+                s += f"$ track {pid} {e:.5f} {dirx:.5f} {diry:.5f} {dirz:.5f} 0\n"
+            s += "$ end\n"
 
-    def _ratpac_string(self, evt, i, events):
+            self.outfile.write(s)
+        
+        self.outfile.write("$ stop\n")
+
+    def _write_ratpac_events(self, events):
         """Return RAT-PAC readable HEPEVT-style representation of event for writing to output file.
 
         Input:
@@ -87,30 +85,32 @@ class EventWriter:
         mm = 10       # convert from cm
         ns = 1000000  # convert from ms
 
-        dt = evt.time
-        if i > 0:
-            dt -= events[i - 1].time
+        for (i, evt) in enumerate(events):
+            dt = evt.time
+            if i > 0:
+                dt -= events[i - 1].time
 
-        s = f"{len(evt.outgoing_particles)}\n"
-        for idx, (pid, e, dirx, diry, dirz) in enumerate(evt.outgoing_particles):
-            if pid == 11 or pid == -11:
-                mass = 0.5109907
-            elif pid == 2112:
-                mass = 939.56563
-            elif pid == 2212:
-                mass = 938.27205
-            else:
-                mass = 0.0
-            p = (e**2 - mass**2)**0.5
-            px = dirx * p
-            py = diry * p
-            pz = dirz * p
-            if idx > 0:
-                dt = 0.0
-            s += f"1 {pid} 0 0 {px * GeV:.8e} {py * GeV:.8e} {pz * GeV:.8e} {mass * GeV:.8e} {dt * ns:.5e} {evt.vertex[0] * mm:.5e} {evt.vertex[1] * mm:.5e} {evt.vertex[2] * mm:.5e}\n"
-        return s
+            s = f"{len(evt.outgoing_particles)}\n"
+            for idx, (pid, e, dirx, diry, dirz) in enumerate(evt.outgoing_particles):
+                if pid == 11 or pid == -11:
+                    mass = 0.5109907
+                elif pid == 2112:
+                    mass = 939.56563
+                elif pid == 2212:
+                    mass = 938.27205
+                else:
+                    mass = 0.0
+                p = (e**2 - mass**2)**0.5
+                px = dirx * p
+                py = diry * p
+                pz = dirz * p
+                if idx > 0:
+                    dt = 0.0
+                s += f"1 {pid} 0 0 {px * GeV:.8e} {py * GeV:.8e} {pz * GeV:.8e} {mass * GeV:.8e} {dt * ns:.5e} {evt.vertex[0] * mm:.5e} {evt.vertex[1] * mm:.5e} {evt.vertex[2] * mm:.5e}\n"
+            
+            self.outfile.write(s)
 
-    def _juno_string(self, orig_evt, i, outfile):
+    def _write_juno_events(self, events):
 
         class EVENT():
             def __init__(self):
@@ -125,37 +125,41 @@ class EventWriter:
                 self.origPDGID = 0
                 self.channel = 0
 
-        evt = EVENT()
+        self.outfile.mktree("SNEvents",{"nparticles": "uint64", "origPDGID":"int32", "nuE":"double", "pdgid": ("int32",(2,)),"t": ("float64",(2,)),
+                                        "px": ("float64",(2,)),"py":("float64",(2,)),"pz":("float64",(2,)),"m":("float64",(2,)), "channel": "int64"})
 
-        for idx, (pid, e, dirx, diry, dirz) in enumerate(orig_evt.outgoing_particles):
-            if pid == 11 or pid == -11:
-                mass = 0.5109907
-            elif pid == 2112:
-                mass = 939.56563
-            elif pid == 2212:
-                mass = 938.27205
-            else:
-                mass = 0.0
-            p = (e**2 - mass**2)**0.5
-            evt.px[idx] = dirx * p
-            evt.py[idx] = diry * p
-            evt.pz[idx] = dirz * p
-            evt.m[idx] = mass
-            evt.pdgid[idx] = pid
+        for orig_evt in events:
+            evt = EVENT()
 
-        if len(orig_evt.outgoing_particles) < 2:
-            # is elastic scattering, second particle is a neutrino, not visible 
-            evt.px[1] = 0
-            evt.py[1] = 0
-            evt.pz[1] = 0
-            evt.m[1] = 0
-            evt.pdgid[1] = 0
+            for idx, (pid, e, dirx, diry, dirz) in enumerate(orig_evt.outgoing_particles):
+                if pid == 11 or pid == -11:
+                    mass = 0.5109907
+                elif pid == 2112:
+                    mass = 939.56563
+                elif pid == 2212:
+                    mass = 938.27205
+                else:
+                    mass = 0.0
+                p = (e**2 - mass**2)**0.5
+                evt.px[idx] = dirx * p
+                evt.py[idx] = diry * p
+                evt.pz[idx] = dirz * p
+                evt.m[idx] = mass
+                evt.pdgid[idx] = pid
 
-        evt.nparticles = len(orig_evt.outgoing_particles)
-        evt.nuE = orig_evt.incoming_particles[0][1]
-        evt.t = [orig_evt.time*1e6, 0]
-        evt.origPDGID = orig_evt.incoming_particles[0][0]
-        evt.channel = orig_evt.code
-        
-        self.outfile["SNEvents"].extend({"pdgid": [evt.pdgid],"px":[evt.px],"py":[evt.py],"pz":[evt.pz],"t":[evt.t],"m":[evt.m],
-                            "nuE":[evt.nuE], "nparticles":[evt.nparticles], "origPDGID":[evt.origPDGID], "channel":[evt.channel]})
+            if len(orig_evt.outgoing_particles) < 2:
+                # is elastic scattering, second particle is a neutrino, not visible 
+                evt.px[1] = 0
+                evt.py[1] = 0
+                evt.pz[1] = 0
+                evt.m[1] = 0
+                evt.pdgid[1] = 0
+
+            evt.nparticles = len(orig_evt.outgoing_particles)
+            evt.nuE = orig_evt.incoming_particles[0][1]
+            evt.t = [orig_evt.time*1e6, 0]
+            evt.origPDGID = orig_evt.incoming_particles[0][0]
+            evt.channel = orig_evt.code
+            
+            self.outfile["SNEvents"].extend({"pdgid": [evt.pdgid],"px":[evt.px],"py":[evt.py],"pz":[evt.pz],"t":[evt.t],"m":[evt.m],
+                                "nuE":[evt.nuE], "nparticles":[evt.nparticles], "origPDGID":[evt.origPDGID], "channel":[evt.channel]})
